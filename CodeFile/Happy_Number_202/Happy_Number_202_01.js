da016c391ff9d206581ec2e84057f4f9d4814eb6
EdwardShiung <edwardshiung@gmail.com>
2023-09-05 14:03:28 -0400

    1st try to solve leetcode_202 by Javascript

---
+++ CodeFile/Happy_Number_202/Happy_Number_202_01.js
@@ -0,0 +1,70 @@
+
+
+//[1st]
+const getSum = num => {
+    let total = 0;
+
+    //Understanding how to manipulate the number operation
+    while(num != 0){
+        let lastDigit = num % 10;
+        num = Math.floor(num / 10);
+        total += Math.pow(lastDigit, 2);
+    }
+    return total;
+}
+
+
+var isHappy = function(n) {
+
+    let set = new Set();
+
+    while(n !== 1){
+        n = getSum(n);
+        if(n !==1 && set.has(n)){
+            console.log(false);
+            return false;
+        }
+        set.add(n);
+    }
+    console.log(true);
+    return true;
+};
+
+isHappy(33);
+
+
+/*
+
+    1. I still don't understand why this solution can be corrected.
+        const getSum = num => {
+            let total = 0;
+
+            while(num != 0){
+                let lastDigit = num % 10;
+                num = Math.floor(num / 10);
+                total += Math.pow(lastDigit, 2);
+            }
+            return total;
+        }
+
+
+        var isHappy = function(n) {
+            let fast = n;
+            let slow = n;
+            while(1){
+                console.log("come")
+                fast = getSum(getSum(fast));
+                slow = getSum(slow);
+                if(fast === slow){
+                    console.log(true);
+                    return fast === 1;
+                }
+            }
+            console.log(false);
+            return false;
+        };
+    --> Above solution is from youtuber, Andy Gala.
+
+    2. Understanding how to manipulate the number operation.
+
+ */
