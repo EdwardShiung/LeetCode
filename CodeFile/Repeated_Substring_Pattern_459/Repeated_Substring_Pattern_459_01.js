85e7edef063957fb09f5297046ee9447164d06a1
EdwardShiung <edwardshiung@gmail.com>
2023-09-21 01:45:38 -0400

    1st try to solve leetcode_459 by Javascript

---
+++ CodeFile/Repeated_Substring_Pattern_459/Repeated_Substring_Pattern_459_01.js
@@ -0,0 +1,72 @@
+//[1st]:
+
+/*
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ */
+
+var repeatedSubstringPattern = function(s) {
+
+    let len = s.length;
+
+    if(len === 0){
+        console.log(false);
+        return false;
+    }
+
+
+    for(let subStrlength = 1; subStrlength <= len / 2; subStrlength  ++){
+
+        //only remainder === 0 才進入 if
+
+        if(len % subStrlength === 0){
+
+            //rep check
+            const rep = (len / subStrlength);
+            //prepare to store the possible string
+            let subString = '';
+
+            //store substring
+            for(let i = 0; i < subStrlength; i ++){
+                subString += s[i];
+            }
+
+
+            //判斷是否為重複子字串
+            let booleam = true;
+
+
+            //verify is repeated or not
+            //知道有幾組後，只要判斷最後一組即可！！
+            for(let i = 1; i < rep; i ++){
+                for(let j = 0; j < subStrlength; j ++){
+                    if(s[i * subStrlength + j] !== subString[j]){
+                        booleam = false;
+                        break;
+                    }
+                }
+                if (!booleam) {
+                    break;
+                }
+
+            }
+
+            if(booleam === true){
+                console.log(true);
+                return true;
+            }
+        }
+
+    }
+    console.log(false);
+    return false;
+
+};
+
+
+let s = 'aba';
+repeatedSubstringPattern(s);
