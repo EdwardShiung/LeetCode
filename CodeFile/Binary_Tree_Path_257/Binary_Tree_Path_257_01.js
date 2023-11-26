e66d71c425a52785099d292c9112fe25e42c12f7
EdwardShiung <edwardshiung@gmail.com>
2023-11-25 23:16:29 -0500

    Update

---
+++ CodeFile/Binary_Tree_Path_257/Binary_Tree_Path_257_01.js
@@ -0,0 +1,12 @@
+//[1st]:
+
+//ES5
+function TreeNode(val, left, right){
+    this.val = val === undefined ? 0 : this.val;
+    this.left = left === undefined ? null : this.left;
+    this.right = right === undefined ? null : this.right;
+}
+
+var binaryTreePaths = function(root){
+
+}
