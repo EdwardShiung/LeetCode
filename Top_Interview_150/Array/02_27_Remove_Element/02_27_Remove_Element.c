f61974f73f1e7259e8a1987da512e44c28dfbdb2
EdwardShiung <edwardshiung@gmail.com>
2024-06-03 09:49:49 -0400

    150 Top Interview Question 02_27_Remove_Element_02

--- Top_Interview_150/Array/02_27_Remove_Element/02_27_Remove_Element.c
+++ Top_Interview_150/Array/02_27_Remove_Element/02_27_Remove_Element.c
@@ -35,22 +35,71 @@ int main(){
 
 int removeElement(int* nums, int numsSize, int val){
 
-    int tail = numsSize - 1;
-
-    printf("%d", tail);
-    printf("\n");
-
-    int begin = 0;
-
-    while(begin <= tail){
-        if(nums[begin] == val){
-            nums[begin] = nums[tail];
-            tail --;
-        }else{
-            begin ++;
+    int slow = 0;
+    for(int fast = 0; fast < numsSize; fast ++){
+        if(nums[fast] !=  val){
+            nums[slow] = nums[fast];
+            slow ++;
         }
     }
-    printf("%d", tail + 1);
-    return tail + 1 ;
+    printf("%d", slow);
+    return slow;
 }   
 
+/*
+    [Thought]:
+    1st:
+        [Approach_01]: Double Pointers
+        1. In the first time, use double pointers to complete the problem.
+        2. set two different pointers
+            - tail pointer      (Check the remaining array size)
+            - beginning pointer
+        
+        once the beginning pointer find out the target number, 
+            beginning pointer = tail
+            tail --;
+
+        so, the result is return the tail
+
+        int removeElement(int* nums, int numsSize, int val){
+
+            int tail = numsSize - 1;
+
+            printf("%d", tail);
+            printf("\n");
+
+            int begin = 0;
+
+            while(begin <= tail){
+                if(nums[begin] == val){
+                    nums[begin] = nums[tail];
+                    tail --;
+                }else{
+                    begin ++;
+                }
+            }
+            printf("%d", tail + 1);
+            return tail + 1 ;
+        }   
+    
+    2nd:
+        [Approach_02]: Double Pointers
+        2. set two different pointers
+            fast (Find the target number)
+            slow (Record the size of array)
+        if find the target numer, slow will not move until the next round.
+
+
+        int removeElement(int* nums, int numsSize, int val){
+
+            int slow = 0;
+            for(int fast = 0; fast < numsSize; fast ++){
+                if(nums[fast] !=  val){
+                    nums[slow] = nums[fast];
+                    slow ++;
+                }
+            }
+            printf("%d", slow);
+            return slow;
+        }   
+*/
