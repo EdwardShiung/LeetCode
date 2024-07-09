de6ade928624f46ef7e0a2484743551ef969eb2c
EdwardShiung <edwardshiung@gmail.com>
2024-07-08 23:50:43 -0400

    BinaryTree_02_94_Binary_Tree_Inorder_Traversal_01

---
+++ Top_Interview/BinaryTree/03_94_Binary_Tree_Inorder_Traversal/03_94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+
+    The classic binary tree problem.
+    Here I categorize the type of binary tree problem.
+    1. Preorder Traversal --> LeetCode 144
+    2. Inorder Traversal  --> LeetCode 94
+    3. PostOrder Traversal --> LeetCode 145  
+
+    [Review] 
+    1. Review the 3 requirements in recursive method.
+        - 確定 Recursive Function 參數 和 返回值
+        - 終止條件
+        - 確定單層邏輯
+*/
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {};
+};
+
+
+class Solution {
+public:
+
+    void traversal(TreeNode* cur, vector<int>& vec) {
+        if(cur == NULL) return;
+        // left
+        traversal(cur->left, vec);
+        // middle
+        vec.push_back(cur->val);
+        //right
+        traversal(cur->right, vec);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> results;
+        traversal(root, results);
+        return results;
+    };
+};
+
