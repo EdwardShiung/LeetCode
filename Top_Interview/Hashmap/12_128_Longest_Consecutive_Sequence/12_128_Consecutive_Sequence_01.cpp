d176614d10b13cdb42b162769ebea50086992b48
EdwardShiung <edwardshiung@gmail.com>
2024-12-07 05:33:42 -0500

    LeetCode_12_128_Consecutive_Sequence_01

--- Top_Interview/Hashmap/12_128_Longest_Consecutive_Sequence/12_128_Consecutive_Sequence_01.cpp
+++ Top_Interview/Hashmap/12_128_Longest_Consecutive_Sequence/12_128_Consecutive_Sequence_01.cpp
@@ -25,31 +25,48 @@ Constraints:
 0 <= nums.length <= 105
 -109 <= nums[i] <= 109
 
+
+    [Thought]:
+    1. Using the 3 nested loop. (Brute Force)
+    2. The time complexity is O(N^3), which will be time limited exceeded(TLE).
+    3. The space complexity is O(1).
+
 */
 
+
 class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int result = 0;
-        if(nums.size() <= 1) return nums.size();
-
-        // Sorted the nums array
-        sort(nums.begin(), nums.end());
-
-        //Using the for-loop to get the result
-        // Record the consecutiveSequence
-        int currSequence = 1;
-        for(int i = 1; i < nums.size(); i++) {
-            // 只處理前後不同的數值！
-            if(nums[i] != nums[i - 1]) {
-                if(nums[i] == nums[i - 1] + 1) {
-                    currSequence++;
-                }else {
-                    result = max(result, currSequence);
-                    currSequence = 1;
-                }
+    // 使用這個 private function 原因在於：當前元素為 n 時，希望找到陣列 nums 中，有 n + 1 這個數值
+    // 因此，target 就是在找尋陣列中，下一個數值。
+private:
+    bool longestConsecutive(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                return true;
             }
         }
-        return max(result, currSequence);
+        return false;
+    }
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // Edge Cases
+        if(nums.size() <= 1) return nums.size();
+        // Set a Result Value (Get the longest length)
+        int result = 0;
+
+        // Using the loop to find the longest consecutive value
+        for(int num : nums) {
+            // currentValue
+            int currentVal = num;
+            // currentConsecutiveSequence
+            int currentConsecutiveSequence = 1;
+            // Using while loop to find the Next Value
+            // While Limitation is: whether could finding the next value  or not
+            while(longestConsecutive(nums, currentVal + 1)) {
+                currentConsecutiveSequence += 1;
+                currentVal += 1;
+            }
+            result = max(result, currentConsecutiveSequence);
+        }
+        return result;
     }
 };
