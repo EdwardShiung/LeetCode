281090fe2c82fa9873c46c4e4a014b91968401ce
EdwardShiung <edwardshiung@gmail.com>
2024-12-07 05:34:19 -0500

    LeetCode_12_128_Consecutive_Sequence_02

--- Top_Interview/Hashmap/10_49_Group_Anagrams/10_49_Group_Anagrams_01.cpp
+++ Top_Interview/Hashmap/10_49_Group_Anagrams/10_49_Group_Anagrams_01.cpp
@@ -71,4 +71,4 @@ class Solution {
         // Return a results
         return results;
     }
-}
+};
---
+++ Top_Interview/Hashmap/12_128_Longest_Consecutive_Sequence/12_128_Consecutive_Sequence_02.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+
+    [Thought]:
+    1. Using Sorted Array to get the longest sequences. (Brute Better Approach)
+        - The method is optimized approach for Method 1.
+    2. The time complexity is O(NlogN).
+        - The time complexity of sort method in C++ or Python is O(NlogN).
+        - O(NlogN) is bigger than O(N).
+    3. The space complexity is O(1).
+
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int result = 0;
+        // edge cases
+        if(nums.size() <= 1) return nums.size();
+
+        // Sorted the nums array
+        sort(nums.begin(), nums.end());
+
+        //Using the for-loop to get the result
+        // Record the consecutiveSequence
+        int currSequence = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            // 只處理前後不同的數值！
+            if(nums[i] != nums[i - 1]) {
+                if(nums[i] == nums[i - 1] + 1) {
+                    currSequence++;
+                }else {
+                    result = max(result, currSequence);
+                    currSequence = 1;
+                }
+            }
+        }
+        return max(result, currSequence);
+    }
+};
