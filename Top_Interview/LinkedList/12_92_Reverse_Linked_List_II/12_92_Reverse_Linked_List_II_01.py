de651595f48d825c8a1f639f4e2018fcea956e82
EdwardShiung <edwardshiung@gmail.com>
2024-12-16 23:15:36 -0500

    Leetcode_12_92_Linked_List_II_01 Python

---
+++ Top_Interview/LinkedList/12_92_Reverse_Linked_List_II/12_92_Reverse_Linked_List_II_01.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head:Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, next)
+        
+        # 1.Reach curr node at postion "Left"
+        leftPrev, cur = dummy, head
+        for i in range(left - 1):
+            leftPrev, cur = cur, cur.next
+        
+        # Reverse from left to right
+        prev = None
+        for i in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev, cur = cur, tmpNext
+            
+        # Update pointers
+        leftPrev.next.next = cur
+        leftPrev.next = prev
+        
+        return dummy.next
+        
+        
