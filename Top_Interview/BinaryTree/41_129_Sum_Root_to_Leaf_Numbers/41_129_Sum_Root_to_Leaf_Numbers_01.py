58f1af80a3f2d8da50712ff9a3b7e664dced5752
EdwardShiung <edwardshiung@gmail.com>
2025-01-09 16:36:47 +0800

    LeetCode 129 Sum Root to Leaf Numbers

---
+++ Top_Interview/BinaryTree/41_129_Sum_Root_to_Leaf_Numbers/41_129_Sum_Root_to_Leaf_Numbers_01.py
@@ -0,0 +1,71 @@
+'''
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+'''
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sumNumbers(self, root:Optional[TreeNode]) -> None:
+        # Base Case
+        if not root:
+            return 
+        # DFS Method by using Stack
+        stack = []
+        result = 0
+        
+        stack.append(root)
+        
+        # DFS 處理
+        while stack:
+            node = stack.pop()
+            # 中間處理
+            if not node.left and not node.right:
+                result += node.val
+            # 左邊處理
+            if node.left:
+                node.left.val += 10 * (node.val)
+                stack.append(node.left)
+            # 右邊處理：
+            if node.right:
+                node.right.val += 10 * (node.val)
+                stack.append(node.right)
+        
+        return result
+                
+            
+        
+        
