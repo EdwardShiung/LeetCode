fd9f851ff8255e36e5255bba2ff2925d5c03b1a0
EdwardShiung <edwardshiung@gmail.com>
2025-01-13 17:22:02 +0800

    LeetCode 74 Search a 2D Matrix

---
+++ Top_Interview/BinarySearch/01_74_Search_a_2D_Matrix/01_74_Search_a_2D_Matrix_01.py
@@ -0,0 +1,64 @@
+'''
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+ 
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+
+[Thought]
+1. 本題不要想 2D 矩陣，而是想成一維陣列
+
+'''
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Edge Case
+        if not matrix or target is None:
+            return False
+        # Get the boundary
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        low = 0
+        high = rows * cols - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            num = matrix[mid // cols][mid % cols]
+            
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            elif num > target:
+                high = mid - 1
+        
+        return False
+    
+sol = Solution()
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 3
+
+result = sol.searchMatrix(matrix, target)
+print(result)
+        
+    
